54ea6ff4a30bfb83e7dd48e1fb78162a1f05e8dc
thiago-sbrito <thiagobritosv@gmail.com>
2020-10-29 21:25:33 -0300

    início do scraping na tabela de múltiplos

--- stocks_scraping.py
+++ stocks_scraping.py
@@ -21,3 +21,26 @@ print(f"\nAsset name: {asset_name}\n"
     f"Price: {price} BRL\n"
     f"Today's variation: {variation}\n"
 )
+
+url_statistics = f"https://finance.yahoo.com/quote/{ticker}.SA/key-statistics"
+request_statistics = requests.get(url_statistics).text
+html_statistics = BeautifulSoup(request_statistics, 'lxml')
+
+header_html = html_statistics.table.find_all("th")
+header = []
+for column in header_html:
+    header.append(column.text)
+else:
+    header[0] = 'Multiple'
+print(header)
+
+index_html = html_statistics.tbody.find_all("tr")
+for i in index_html:
+   print(i.get_text(separator = ','))
+
+#header vai ser utilizado para ser as columns do pandas
+#In [51]: pd.DataFrame(data, columns=['C', 'A', 'B'])
+#Out[51]: 
+#          C  A    B
+#0  b'Hello'  1  2.0
+#1  b'World'  2  3.0
